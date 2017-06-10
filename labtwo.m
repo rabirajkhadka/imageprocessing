0d0835b569b197865f252a91880d822243040bfa
Rabi Raj Khadka <er.rabirajkhadka@gmail.com>
2017-06-10 19:32:48 +0545

    Lab Two: Image operations like negative, grayscale,B&W

---
+++ labtwo.m
@@ -0,0 +1,55 @@
+%Title: Introduction to basic image operation using MATLAB
+%Author:Rabi Raj Khadka
+%---------------------------------------------------------
+%Three Critical Statements 
+%---------------------------------------------------------
+close all;
+clear variables;
+clc;
+%---------------------------------------------------------
+%Operations: Input
+%---------------------------------------------------------
+kheclogo=imread('C:\Users\rabiraj\Desktop\ImageProcessingLab\img\UCT.png');
+
+%---------------------------------------------------------
+%Operations: Manipulation
+%---------------------------------------------------------
+%negkheclogo=255-kheclogo;
+%imwrite(negkheclogo,'negativekhec.jpg');
+negative_kheclogo=255-kheclogo;
+grayscale_kheclogo=rgb2gray(kheclogo);
+grayscale_of_negative_kheclogo=rgb2gray(negative_kheclogo);
+blackandwhie_kheclogo=im2bw(kheclogo);
+negative_of_blackandwhite_kheclogo=1-im2bw(kheclogo);
+
+%---------------------------------------------------------
+%Operations: Output
+%---------------------------------------------------------
+%imshow(kheclogo);
+%imshow(255-kheclogo);
+%imshow(rgb2gray(kheclogo));
+%imshow(rgb2gray(negkheclogo));
+%imshow(im2bw(kheclogo));
+%imshow(1-im2bw(kheclogo));
+subplot(3,2,1);
+imshow(kheclogo);
+title('Original Image');
+subplot(3,2,2);
+imshow(negative_kheclogo);
+title('Negative Image');
+subplot(3,2,3);
+imshow(grayscale_kheclogo);
+title('Grayscale Image');
+subplot(3,2,4);
+imshow(grayscale_of_negative_kheclogo);
+title('Grayscale of Negative');
+subplot(3,2,5);
+imshow(blackandwhie_kheclogo);
+title('B/W of Original Image');
+subplot(3,2,6);
+imshow(negative_of_blackandwhite_kheclogo);
+title('B/W of Negative Image');
+
+
+
+
